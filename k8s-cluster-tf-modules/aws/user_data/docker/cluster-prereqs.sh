71e02671b36c0e45efe7d5884a7756e6d2a27155
Karthick Prabu Ponnusamy <Karthick.Ponnusamy@viacomcontractor.com>
2019-06-24 14:38:45 +0530

    updated docker user-data scripts

--- k8s-cluster-tf-modules/aws/user_data/docker/cluster-prereqs.sh
+++ k8s-cluster-tf-modules/aws/user_data/docker/cluster-prereqs.sh
@@ -1,63 +1,25 @@
 #!/bin/bash
 set -e -o pipefail
 
-sudo add-apt-repository -y ppa:projectatomic/ppa
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) \
+stable"
+
 sudo apt-get update
+sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
+sudo apt-mark hold docker-ce
 
-# cri-o dependencies
-sudo apt-get install -y \
-  btrfs-tools \
-  containers-common \
-  git \
-  golang-go \
-  libassuan-dev \
-  libdevmapper-dev \
-  libglib2.0-dev \
-  libc6-dev \
-  libgpgme11-dev \
-  libgpg-error-dev \
-  libseccomp-dev \
-  libsystemd-dev \
-  libselinux1-dev \
-  pkg-config \
-  go-md2man \
-  cri-o-runc
+sudo systemctl status docker
 
-# install cri-o v1.12
-sudo apt-get install -y skopeo-containers cri-o-1.12
-
-sudo systemctl enable crio
-sudo systemctl start crio
-
-# add k8s repository info
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
-# install kubeadm, kubectl, kubelet
 sudo apt-get update
 sudo apt-get install -y kubelet=1.12.7-00 kubeadm=1.12.7-00 kubectl=1.12.7-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# assuming cgroup driver is systemd, add extra args for kubelet
-# specifying cri-o socket as runtime endpoint
-cat <<EOF | sudo tee /etc/default/kubelet
-KUBELET_EXTRA_ARGS=--feature-gates="AllAlpha=false,RunAsGroup=true" --container-runtime=remote --cgroup-driver=systemd --container-runtime-endpoint='unix:///var/run/crio/crio.sock' --runtime-request-timeout=5m
-EOF
-
-sudo systemctl daemon-reload
-
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-# Setup required sysctl params, these persist across reboots.
-# This is done by default in Docker, but for cri-o need to do this manually
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf 
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF
-
-sudo sysctl --system
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
